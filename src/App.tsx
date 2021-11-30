92e47b5b98544167b24eb0a04867cf6cb97090ce
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-30 19:48:38 +0100

    game setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styled from "styled-components";
+import Setup from "./components/Setup";
 function App() {
-  return <Container></Container>;
+  const [start, startSet] = useState(false);
+  const [players, playersSet] = useState<String[]>([]);
+
+  return (
+    <Container>
+      {!start && <Setup startSet={startSet} playersSet={playersSet} />}
+      {console.log(start)}
+      {start && (
+        <Row>
+          {players.map((player) => {
+            return <div>{player}</div>;
+          })}
+        </Row>
+      )}
+    </Container>
+  );
 }
 
 export default App;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Row = styled.div`
+  display: flex;
+`;
---
+++ src/components/Setup.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from "react";
+import range from "lodash/range";
+type Props = {
+  startSet: React.Dispatch<React.SetStateAction<boolean>>;
+  playersSet: React.Dispatch<React.SetStateAction<String[]>>;
+};
+const Setup: FC<Props> = ({ startSet, playersSet }) => {
+  const [playersCount, playersCountSet] = useState(0);
+  return (
+    <>
+      Ile graczy?
+      <div>
+        {playersCount > 0 ? (
+          <button onClick={() => playersCountSet((prev) => prev - 1)}>-</button>
+        ) : null}
+        {playersCount}
+        <button onClick={() => playersCountSet((prev) => prev + 1)}>+</button>
+      </div>
+      {range(playersCount).map((el) => {
+        return (
+          <input
+            type="text"
+            onChange={(e) =>
+              playersSet((prevArr) => {
+                return Object.assign([], prevArr, { [el]: e.target.value });
+              })
+            }
+          />
+        );
+      })}
+      <button onClick={() => startSet(true)}>Rozpocznij grę</button>
+    </>
+  );
+};
+
+export default Setup;
