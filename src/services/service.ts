cf312d49123f210fb78dbc0168fb14b0f7e9b46d
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-12-13 19:04:29 +0100

    routes

---
+++ src/services/service.ts
@@ -0,0 +1,23 @@
+import firebase from "../firebase";
+
+const db = firebase.ref("/tutorials");
+
+class TutorialDataService {
+  getAll() {
+    return db;
+  }
+
+  update(key: string, value: any) {
+    return db.child(key).update(value);
+  }
+
+  delete(key: string) {
+    return db.child(key).remove();
+  }
+
+  deleteAll() {
+    return db.remove();
+  }
+}
+
+export default new TutorialDataService();
