90b190e1f07a22cad4e1375fcfdc25787cee2b86
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-12-17 20:46:24 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Setup from "./components/Setup";
 import _uniqueId from "lodash/uniqueId";
